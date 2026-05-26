c1fe9574a280ccb246a3cc9508ac264383401472
GabrielGoulartM <gabrielmaia18@gmail.com>
2026-05-25 21:35:02 -0300

    W05 - Obtendo dados dos painéis e usando FormData

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -69,6 +69,19 @@ class WhatsAppController {
     Element.prototype.hasClass = function (name) {
       return this.classList.contains(name);
     };
+
+    HTMLFormElement.prototype.getForm = function () {
+      return new FormData(this);
+    };
+
+    HTMLFormElement.prototype.toJSON = function () {
+      let json = {};
+
+      this.getForm().forEach((value, key) => {
+        json[key] = value;
+      });
+      return json;
+    };
   }
 
   closeAllLeftPanel() {
@@ -78,7 +91,6 @@ class WhatsAppController {
 
   initEvents() {
     // Interações com o painel de MINHA FOTO
-
     this.el.myPhoto.on("click", (e) => {
       //Atribui evento click no iconde de MINHA FOTO
 
@@ -108,6 +120,30 @@ class WhatsAppController {
     //fecha o painel de adicionar contato
     this.el.btnClosePanelAddContact.on("click", (e) => {
       this.el.panelAddContact.removeClass("open");
+    });
+
+    //Interações com o botão de foto
+    this.el.photoContainerEditProfile.on("click", (e) => {
+      this.el.inputProfilePhoto.click(); //função .click esta forçando o evento de click para abrir a janela de selecionar imagem
+    });
+
+    //Interações com os paineis de edição de nome
+    this.el.inputNamePanelEditProfile.on("keypress", (e) => {
+      //atribui evento de click ao div de Nome do usuario
+      if (e.key === "Enter") {
+        e.preventDefault(); // isso impede do navegador dar refresh ao clicar enter
+        this.el.btnSavePanelEditProfile.click(); // força o evento click no botão de salvar o nome de perfil
+      }
+    });
+
+    //Evento de click no botão de confirma
+    this.el.btnSavePanelEditProfile.on("click", (e) => {
+      console.log(this.el.inputNamePanelEditProfile.innerHTML); // printa o valor dentro do elemento HTML
+    });
+
+    this.el.formPanelAddContact.on("submit", (e) => {
+      e.preventDefault();
+      let formData = new FormData(this.el.formPanelAddContact);
     });
   }
 }
