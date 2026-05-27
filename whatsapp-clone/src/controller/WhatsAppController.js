94ee9b21d02f06b875f853e1697e4932003f14eb
GabrielGoulartM <gabrielmaia18@gmail.com>
2026-05-27 08:09:42 -0300

    W10 - Eventos do campo Digitar Mensagem

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -251,6 +251,41 @@ class WhatsAppController {
     this.el.btnFinishMicrophone.on("click", (e) => {
       this.closeRecordMicrophone();
     });
+
+    //Configuração para o comportamento de Enter / CTRL + Enter na caixa de ESCREVER MENSAGEM
+    this.el.inputText.on("keypress", (e) => {
+      if (e.key === "Enter" && !e.ctrlKey) {
+        e.preventDefault();
+        this.el.btnSend.click();
+      }
+    });
+
+    //Interações com o botão de clicar em ESCREVER MENSAGEM
+    this.el.inputText.on("keyup", (e) => {
+      if (this.el.inputText.innerHTML.length) {
+        this.el.inputPlaceholder.hide();
+        this.el.btnSendMicrophone.hide();
+        this.el.btnSend.show();
+      } else {
+        this.el.inputPlaceholder.show();
+        this.el.btnSendMicrophone.show();
+        this.el.btnSend.hide();
+      }
+    });
+
+    this.el.btnSend.on("click", (e) => {
+      console.log(this.el.inputText.innerHTML);
+    });
+
+    this.el.btnEmojis.on("click", (e) => {
+      this.el.panelEmojis.toggleClass("open");
+    });
+
+    this.el.panelEmojis.querySelectorAll(".emojik").forEach((emoji) => {
+      emoji.on("click", (e) => {
+        console.log(emoji.dataset.unicode);
+      });
+    });
   }
 
   //Método para fechar todos os paineis do botão de attechment
