d010033dee2cc6e6cb6732590cadd5a187bd557d
GabrielGoulartM <gabrielmaia18@gmail.com>
2026-05-28 07:49:52 -0300

    W13 - Ativando a câmera com API MediaDevices.getUserMedia()

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -196,6 +196,7 @@ class WhatsAppController {
       this.el.panelCamera.css({
         height: "calc(100% - 120px)",
       });
+      this._camera = new CameraController(this.el.videoCamera);
     });
 
     this.el.btnClosePanelCamera.on("click", (e) => {
@@ -284,20 +285,18 @@ class WhatsAppController {
     this.el.panelEmojis.querySelectorAll(".emojik").forEach((emoji) => {
       emoji.on("click", (e) => {
         console.log(emoji.dataset.unicode);
-        
+
         let img = this.el.imgEmojiDefault.cloneNode();
         img.style.cssText = emoji.style.cssText;
         img.dataset.unicode = emoji.dataset.unicode;
         img.alt = emoji.dataset.unicode;
 
-        emoji.classList.forEach(name=>{
+        emoji.classList.forEach((name) => {
           img.classList.add(name);
-        })
+        });
 
-        
-        
         let cursor = window.getSelection();
-        if(!cursor.focusNode || !cursor.focusNode.id == 'input-text'){
+        if (!cursor.focusNode || !cursor.focusNode.id == "input-text") {
           this.el.inputText.focus();
           cursor = window.getSelection();
         }
@@ -315,15 +314,9 @@ class WhatsAppController {
 
         range.setStartAfter(img);
 
-        this.el.inputText.dispatchEvent(new Event('keyup'));
-
+        this.el.inputText.dispatchEvent(new Event("keyup"));
       });
     });
-
-
-    
-
-
   }
 
   //Método para fechar todos os paineis do botão de attechment
