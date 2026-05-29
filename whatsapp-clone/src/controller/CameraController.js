4263bab3711e068aa197f1009dd728ca3379ab8b
Gabriel Goulart <gabrielmaia18@gmail.com>
2026-05-28 21:07:54 -0300

    W15.2 - Ajustando propriedades da camera

--- whatsapp-clone/src/controller/CameraController.js
+++ whatsapp-clone/src/controller/CameraController.js
@@ -1,17 +1,19 @@
 export class CameraController {
   constructor(videoEl) {
-    this._videoEl = videoEl;
+    
+    this.videoEl = videoEl; 
 
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-      })
-      .then((stream) => {
-        this._videoEl.src = URL.createObjectURL(stream);
-        this._videoEl.play();
-      })
-      .catch((err) => {
+   
+    navigator.mediaDevices.getUserMedia({
+        video: true
+    }).then(stream => {
+        
+       
+        this.videoEl.srcObject = stream;
+        this.videoEl.play();
+
+    }).catch(err => {
         console.error(err);
-      });
+    });
   }
 }
