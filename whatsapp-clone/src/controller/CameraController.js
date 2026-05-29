723f66ce4dd1644a8b69b4e2c5e03da9c6a087cf
Gabriel Goulart <gabrielmaia18@gmail.com>
2026-05-28 21:19:52 -0300

    W17 - Parando de capturar imagem da câmera - getTracks()

--- whatsapp-clone/src/controller/CameraController.js
+++ whatsapp-clone/src/controller/CameraController.js
@@ -1,19 +1,27 @@
 export class CameraController {
   constructor(videoEl) {
-    
-    this.videoEl = videoEl; 
+    this.videoEl = videoEl;
 
-   
     navigator.mediaDevices.getUserMedia({
-        video: true
-    }).then(stream => {
-        
-       
-        this.videoEl.srcObject = stream;
-        this.videoEl.play();
+      video: true
+    }).then(stream => { 
+      // A partir de stream, acessamos os elementos que estão sendo enviados (video, audio, etc)
+      this._stream = stream;
+      this.videoEl.srcObject = stream;
+      this.videoEl.play();
 
     }).catch(err => {
-        console.error(err);
+      console.error(err);
     });
+  }
+
+  // O método stop deve ficar AQUI, dentro da classe
+  stop() {
+    // Verificação de segurança: só tenta parar se o stream realmente existir
+    if (this._stream) {
+      this._stream.getTracks().forEach(track => {  // Corrigido de -> para =>
+        track.stop();
+      });
+    }
   }
 }
