4f7db74ba4a411aa66df32c2dd2d7913ff3691be
Gabriel Goulart <gabrielmaia18@gmail.com>
2026-06-05 21:44:16 -0300

    MY06 - EJS Include

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -2,33 +2,33 @@ var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* ================================
+   ROTA: HOME (Página Inicial)
+   ================================ */
 router.get('/', function(req, res, next) {
 
   conn.query(`
       SELECT * FROM tb_menus ORDER BY title  
     `, (err, results) => {
 
-      // SE DER ERRO: Mostra o erro e PARA TUDO (não tenta renderizar a página)
       if (err) {
         console.log("ERRO NO BANCO:", err);
         return res.send("Ocorreu um erro ao buscar os menus no banco de dados.");
       }
 
-      // SE DEU TUDO CERTO: O return ali de cima não foi chamado, então renderiza a página
       res.render('index', {
           title: 'Restaurante Saboroso!', 
           menus: results
       });
       
     });
-
 });
 
-/* GET menus page. */
+/* ================================
+   ROTA: MENUS
+   ================================ */
 router.get('/menus', function(req, res, next) {
   
-    // Fazemos a mesma busca no banco de dados para a página de menus
     conn.query(`
         SELECT * FROM tb_menus ORDER BY title  
     `, (err, results) => {
@@ -38,12 +38,41 @@ router.get('/menus', function(req, res, next) {
             return res.send("Ocorreu um erro ao buscar os menus no banco de dados.");
         }
 
-        // Renderiza o arquivo views/menus.ejs (ou view/menu.ejs dependendo de como você nomeou)
         res.render('menus', {
             title: 'Nossos Menus - Restaurante Saboroso!', 
             menus: results
         });
       
+    });
+});
+
+/* ================================
+   ROTA: CONTATOS (Contacts)
+   ================================ */
+router.get('/contacts', function(req, res, next) {
+    // Renderiza o arquivo views/contact.ejs
+    res.render('contacts', {
+        title: 'Contato - Restaurante Saboroso!'
+    });
+});
+
+/* ================================
+   ROTA: RESERVAS (Reservations)
+   ================================ */
+router.get('/reservations', function(req, res, next) {
+    // Renderiza o arquivo views/reservation.ejs
+    res.render('reservations', {
+        title: 'Reservas - Restaurante Saboroso!'
+    });
+});
+
+/* ================================
+   ROTA: SERVIÇOS (Services)
+   ================================ */
+router.get('/services', function(req, res, next) {
+    // Renderiza o arquivo views/services.ejs
+    res.render('services', {
+        title: 'Serviços - Restaurante Saboroso!'
     });
 });
 
