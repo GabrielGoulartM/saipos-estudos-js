bd732cb5fd0409d1662442032c8efd95c22562a6
Gabriel Goulart <gabrielmaia18@gmail.com>
2026-06-07 23:13:27 -0300

    MY08 - Organizando o FOOTER com Includes

--- saboroso/routes/users.js
+++ saboroso/routes/users.js
@@ -8,26 +8,6 @@ router.get('/', function(req, res, next) {
 
 
 
-/* GET menus page. */
-router.get('/menus', function(req, res, next) {
-  
-    // Fazemos a mesma busca no banco de dados para a página de menus
-    conn.query(`
-        SELECT * FROM tb_menus ORDER BY title  
-    `, (err, results) => {
 
-        if (err) {
-            console.log("ERRO NO BANCO:", err);
-            return res.send("Ocorreu um erro ao buscar os menus no banco de dados.");
-        }
-
-        // Renderiza o arquivo views/menus.ejs (ou view/menu.ejs dependendo de como você nomeou)
-        res.render('menus', {
-            title: 'Nossos Menus - Restaurante Saboroso!', 
-            menus: results
-        });
-      
-    });
-});
 
 module.exports = router;
